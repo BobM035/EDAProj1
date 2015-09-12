7b9d3835f009396c3b70b6b6a1f9a5ddc12d9c21
BobM035 <BobM035@gmail.com>
2015-09-12 16:34:13 -0400

    Fixed reading in data
    factor vs numeric issue

--- plot4.R
+++ plot4.R
@@ -4,21 +4,13 @@ library(lubridate)
 # Read in data file
 # (Data was downloaded from: 
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip)
-powerdf<-read.csv2("bigdatafiles/household_power_consumption.txt")
+powerdf<-read.csv("bigdatafiles/household_power_consumption.txt", sep=";", na.strings = "?")
 
 # Make date more useable
 powerdf$Date<-dmy_hms(paste(powerdf$Date,powerdf$Time))
 
 # subset data to the range requested
 subdf<-subset(powerdf, Date < mdy("2/3/2007") & Date >= mdy("2/1/2007"))
-
-# Make numbers more useable
-subdf$Global_active_power<-as.numeric(subdf[,3])
-subdf$Global_reactive_power<-as.numeric(subdf[,4])
-subdf$Voltage<-as.numeric(subdf[,5])
-subdf$Sub_metering_1<-as.numeric(subdf[,7])
-subdf$Sub_metering_2<-as.numeric(subdf[,8])
-subdf$Sub_metering_3<-as.numeric(subdf[,9])
 
 #Create plots
 png(file='plot4.png')
